aa0b4d201d0039dc23a225cdada36b7967de7b5a
HelloHailie <alsltjsals@gmail.com>
2023-01-06 16:03:06 +0900

    feat:토큰 로컬스토리지에 저장

--- client/src/view/pages/Auth.tsx
+++ client/src/view/pages/Auth.tsx
@@ -27,7 +27,7 @@ const Auth = () => {
           password,
         })
         .then((response) => {
-          sessionStorage.setItem("token", response.data.token);
+          localStorage.setItem("token", response.data.token);
           console.log(response.data.token);
           navigate(`/todo`);
         })
