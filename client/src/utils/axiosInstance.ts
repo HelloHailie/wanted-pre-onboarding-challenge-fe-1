bb3172bd2658758cedb78a8410f0fabe93634d60
HelloHailie <alsltjsals@gmail.com>
2023-01-16 22:08:08 +0900

    refactor:AxiosRequestConfig 사용해서 any 타입 없애기

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 // import { IToken } from "../types/model";
 
@@ -25,10 +25,11 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use(
-  async (config: any) => {
+  async (config: AxiosRequestConfig) => {
     const token = localStorage.getItem("token");
     if (token) {
-      config.headers["Authorization"] = `Bearer ${token}`; //여기는 accessToken
+      config.headers = {};
+      config.headers.Authorization = `Bearer ${token}`; //여기는 accessToken
     }
     return config;
   },
